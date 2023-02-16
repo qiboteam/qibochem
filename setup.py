9dbc8647120f9f3bd6cd3e589c093b42e031dcaf
chmwzc <70616433+chmwzc@users.noreply.github.com>
2023-02-16 11:04:07 +0800

    Small changes for packaging, following qibotn

--- setup.py
+++ setup.py
@@ -1,18 +1,31 @@
 # Installation script for python
 from setuptools import setup, find_packages
-import os
 import re
+import pathlib
 
+HERE = pathlib.Path(__file__).parent.absolute()
 PACKAGE = "qibochem"
 
-# load long description from README
-this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
 
+# Returns the qibotn version
+def version():
+    """Gets the version from the package's __init__ file
+    if there is some problem, let it happily fail"""
+    version_file = HERE / "src" / PACKAGE / "__init__.py"
+    version_regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
+
+    initfile = version_file.read_text(encoding="utf-8")
+    matched = re.search(version_regex, initfile, re.M)
+
+    if matched is not None:
+        return matched.group(1)
+    return "0.0.0"
+
+
+# load long description from README
 setup(
     name="qibochem",
-    version="0.1",
+    version=version(),
     description="A chemistry module for quantum computing",
     author="The Qibo team",
     author_email="",
@@ -27,15 +40,26 @@ setup(
         "Topic :: Scientific/Engineering :: Chemistry",
     ],
     install_requires=[
-        "pyscf",
+        "qibo>=0.1.10",
         "openfermion",
-        "qibo"
+        # "pyscf",
     ],
+    # extras_require={
+    #     "docs": ["sphinx", "sphinx_rtd_theme", "recommonmark", "sphinxcontrib-bibtex", "sphinx_markdown_tables", "nbsphinx", "IPython", "doc2dash>=2.4.1", ],
+    #     "tests": ["pytest", "cirq", "ply", "sklearn", "dill", "coverage", "pytest-cov"],
+    # }
     extras_require={
-        "docs": ["sphinx", "sphinx_rtd_theme", "recommonmark", "sphinxcontrib-bibtex", "sphinx_markdown_tables", "nbsphinx", "IPython", "doc2dash>=2.4.1", ],
-        "tests": ["pytest", "cirq", "ply", "sklearn", "dill", "coverage", "pytest-cov"],
+        "docs": [],
+        "tests": [
+            "pytest>=7.2.0",
+            "pytest-cov>=4.0.0",
+            "pytest-env>=0.8.1",
+        ],
+        "analysis": [
+            "pylint>=2.16.0",
+        ],
     },
     python_requires=">=3.7.0",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description=(HERE / "README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
 )
