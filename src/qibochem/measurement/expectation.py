4bba665a31614d70f30aff026b4e6cedb9957ab4
chmwzc <70616433+chmwzc@users.noreply.github.com>
2023-03-15 12:06:13 +0800

    Fix bug in expectation.py
    Typo: pauli_expectation should have been pauli_expectation_shots

--- src/qibochem/measurement/expectation.py
+++ src/qibochem/measurement/expectation.py
@@ -1,15 +1,18 @@
-import qibo
-from qibo import gates
-from qibo.models import Circuit
-from basis_rotate import measure_rotate_basis
+
+# from qibo import gates
+# from qibo.models import Circuit
+
+from qibochem.measurement.basis_rotate import measure_rotate_basis
 
 def pauli_expectation_shots(qc, nshots):
     '''
-    calculates expectation value of circuit for pauli string from shots
-    inputs:
+    Calculates expectation value of circuit for pauli string from shots
+
+    Args:
         qc: quantum circuit with appropriate rotations and measure gates
         nshots: number of shots
-    outputs:
+
+    Returns:
         expectation: expectation value
     '''
     result = qc.execute(nshots=nshots)
@@ -17,29 +20,30 @@ def pauli_expectation_shots(qc, nshots):
 
     bitcount = 0
     for bitstring, count in meas.items():
-        if bitstring.count('1')%2 == 0:
+        if bitstring.count('1') % 2 == 0:
             bitcount += count
-        else: 
+        else:
             bitcount -= count
     expectation = bitcount/nshots
     return expectation
 
 def circuit_expectation_shots(qc, of_qubham, nshots=1000):
     '''
-    calculates expectation value of circuit for qubit hamiltonian from shots
-    args:
-        qc: quantum circuit with ansatz
-        of_qubham: qubit hamiltonian in openfermion format
-    returns:
-        expectation value of circuit 
-        
+    Calculates expectation value of qubit hamiltonian w.r.t. circuit ansatz using shots
+
+    Args:
+        qc: Quantum circuit with ansatz
+        of_qubham: Molecular Hamiltonian given as an OpenFermion QubitOperator 
+
+    Returns:
+        Expectation value of of_qubham w.r.t. circuit ansatz
     '''
-    nterms = len(of_qubham.terms)
+    # nterms = len(of_qubham.terms)
     nqubits = qc.nqubits
     # print(nterms)
     coeffs = []
     strings = []
-    
+
     expectation = 0
     for qubop in of_qubham.terms:
         coeffs.append(of_qubham.terms[qubop])
@@ -51,16 +55,15 @@ def circuit_expectation_shots(qc, of_qubham, nshots=1000):
         if len(pauli_op[0]) == 0: # no pauli obs to measure,
             expectation += coeffs[istring]
             # print(coeffs[istring])
-        else: 
+        else:
             # add rotation gates to rotate pauli observable to computational basis
             # i.e. X to Z, Y to Z
             meas_qc = measure_rotate_basis(pauli_op[0], nqubits)
             full_qc = qc + meas_qc
             # print(full_qc.draw())
-            pauli_op_exp = pauli_expectation(full_qc, nshots)
+            pauli_op_exp = pauli_expectation_shots(full_qc, nshots)
             expectation += coeffs[istring]*pauli_op_exp
             ## print(pauli_op, pauli_op_exp, coeffs[istring])
         istring += 1
 
     return expectation
-
