224d9ca5e641481ce8070c0ff291651de0df18c2
Adrian Mak <damarkian@gambit.lan>
2023-07-06 15:29:38 +0800

    Added sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,47 +1,24 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath("../src"))
-
-
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "Qibochem"
-copyright = "2023, The Qibo team"
-author = "The Qibo team"
-
+project = 'qibochem'
+copyright = '2023, qiboteam'
+author = 'qiboteam'
+release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.doctest",
-    "sphinx.ext.coverage",
-    "sphinx.ext.napoleon",
-]
+extensions = ['sphinxcontrib.katex']
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+templates_path = ['_templates']
 exclude_patterns = []
+
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -51,7 +28,20 @@ exclude_patterns = []
 #
 html_theme = "furo"
 
+# custom title
+html_title = "Version " + release
+
+# custom html theme options (colors and font)
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#6400FF",
+        "color-brand-secondary": "#6400FF",
+        "color-brand-content": "#6400FF",
+    }
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+
 html_static_path = ["_static"]
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,18 +1,34 @@
 .. title::
-      Qibochem
+   Qibochem
 
-Welcome to Qibochem's documentation!
-====================================
+What is Qibochem?
+=================
+
+Qibochem is a plugin to Qibo for quantum chemistry simulations. 
+
+Qibochem key features:
+
+* General purpose Molecule class
+  
+  - effective Hamiltonian construction for active space 
+  - inline or .xyz file input for molecular geometry
+  
+* PySCF or Psi4 for calculation of 1- and 2-electron integrals
+* Unitary Coupled Cluster Ansatz
+
+Contents
+========
 
 .. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+    :maxdepth: 2
+    :caption: Introduction
 
+    getting-started/index
 
+.. toctree:: 
+    :maxdepth: 2
+    :caption: Main Documentation
 
-Indices and tables
-==================
+    api-reference/index
+    developer-guides/index
 
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
