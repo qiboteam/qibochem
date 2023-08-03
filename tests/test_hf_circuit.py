f3a78a452965e332fda1de151e64371d96b0ad6e
= <tanle4930@gmail.com>
2023-08-03 09:21:22 +0800

    test fixes

--- tests/test_hf_circuit.py
+++ tests/test_hf_circuit.py
@@ -7,6 +7,7 @@ import pytest
 
 from qibochem.ansatz.hf_reference import hf_circuit
 from qibochem.driver.molecule import Molecule
+from qibochem.measurement.expectation import expectation
 
 
 def test_jw_circuit():
@@ -25,7 +26,7 @@ def test_jw_circuit():
 
     # Molecular Hamiltonian and the HF expectation value
     hamiltonian = h2.hamiltonian()
-    hf_energy = h2.expectation(circuit, hamiltonian)
+    hf_energy = expectation(circuit, hamiltonian)
 
     # assert h2.e_hf == pytest.approx(hf_energy)
     assert h2_ref_energy == pytest.approx(hf_energy)
@@ -47,7 +48,7 @@ def test_bk_circuit_1():
 
     # Molecular Hamiltonian and the HF expectation value
     hamiltonian = h2.hamiltonian(ferm_qubit_map="bk")
-    hf_energy = h2.expectation(circuit, hamiltonian)
+    hf_energy = expectation(circuit, hamiltonian)
 
     # assert h2.e_hf == pytest.approx(hf_energy)
     assert h2_ref_energy == pytest.approx(hf_energy)
@@ -67,6 +68,6 @@ def test_bk_circuit_2():
 
     # Molecular Hamiltonian and the HF expectation value
     hamiltonian = lih.hamiltonian(ferm_qubit_map="bk")
-    hf_energy = lih.expectation(circuit, hamiltonian)
+    hf_energy = expectation(circuit, hamiltonian)
 
     assert lih.e_hf == pytest.approx(hf_energy)
