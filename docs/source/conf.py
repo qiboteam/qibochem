a8c13cadd9c6197165508ce20c7657b297a38eef
Wong Zi Cheng <70616433+chmwzc@users.noreply.github.com>
2023-09-07 04:33:16 +0000

    Draft API documentation for Molecule class

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,19 +3,40 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+from recommonmark.transform import AutoStructify
+
+sys.path.insert(0, os.path.abspath(".."))
+
+import qibochem
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "qibochem"
-copyright = "2023, qiboteam"
-author = "qiboteam"
+copyright = "2023, The Qibo team"
+author = "The Qibo team"
 release = "0.0.1"
+# release = qibochem.__version__
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+master_doc = "index"
+
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "recommonmark",
     "sphinxcontrib.katex",
 ]
 
@@ -47,3 +68,21 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 
 html_static_path = ["_static"]
+
+# -- Autodoc ------------------------------------------------------------------
+#
+autodoc_member_order = "bysource"
+
+
+# Adapted this from
+# https://github.com/readthedocs/recommonmark/blob/ddd56e7717e9745f11300059e4268e204138a6b1/docs/conf.py
+# app setup hook
+def setup(app):
+    app.add_config_value("recommonmark_config", {"enable_eval_rst": True}, True)
+    app.add_transform(AutoStructify)
+    app.add_css_file("css/style.css")
+
+
+# html_logo = "logo.png"
+
+html_show_sourcelink = False
