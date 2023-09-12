e716a259198579c1b6e468da8c90035feb844412
Adrian Mak <3521998+damarkian@users.noreply.github.com>
2023-09-12 11:58:59 +0800

    Generalized the hea ansatz

--- src/qibochem/ansatz/hardware_efficient.py
+++ src/qibochem/ansatz/hardware_efficient.py
@@ -1,26 +1,17 @@
 from qibo import gates
 
+def hea(nlayers, nqubits, parameter_gates=["RY", "RZ"], coupling_gates="CZ"):
 
-def hea_su2(nlayers, nqubits):
-    """
-    implements a hardware-efficient ansatz SU2
-    inputs:
-    """
     gate_list = []
 
     for ilayer in range(nlayers):
-        for iqubit in range(nqubits):
-            gate_list.append(gates.RY(iqubit, theta=0))
-        for iqubit in range(nqubits):
-            gate_list.append(gates.RZ(iqubit, theta=0))
+        for igate in range(len(parameter_gates)):
+            for iqubit in range(nqubits):
+                gate_list.append(getattr(gates, parameter_gates[igate])(iqubit, theta=0))
+    
         # entanglement
         for iqubit in range(nqubits - 1):
-            gate_list.append(gates.CNOT(iqubit, iqubit + 1))
-        gate_list.append(gates.CNOT(nqubits - 1, 0))
-
-    for iqubit in range(nqubits):
-        gate_list.append(gates.RY(iqubit, theta=0))
-    for iqubit in range(nqubits):
-        gate_list.append(gates.RZ(iqubit, theta=0))
+            gate_list.append(getattr(gates, coupling_gates)(iqubit, iqubit + 1))
+        gate_list.append(getattr(gates, coupling_gates)(nqubits - 1, 0))       
 
     return gate_list
