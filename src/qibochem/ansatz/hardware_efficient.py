001f7ac01277042f09bf8aee955c6b9e8e63397a
Wong Zi Cheng <70616433+chmwzc@users.noreply.github.com>
2023-10-06 03:07:14 +0000

    Fix minor bug

--- src/qibochem/ansatz/hardware_efficient.py
+++ src/qibochem/ansatz/hardware_efficient.py
@@ -18,14 +18,14 @@ def hea(n_layers, n_qubits, parameter_gates=["RY", "RZ"], coupling_gates="CZ"):
     """
     gate_list = []
 
-    for ilayer in range(nlayers):
+    for ilayer in range(n_layers):
         for rgate in parameter_gates:
-            for iqubit in range(nqubits):
+            for iqubit in range(n_qubits):
                 gate_list.append(getattr(gates, rgate)(iqubit, theta=0))
 
         # entanglement
-        for iqubit in range(nqubits - 1):
+        for iqubit in range(n_qubits - 1):
             gate_list.append(getattr(gates, coupling_gates)(iqubit, iqubit + 1))
-        gate_list.append(getattr(gates, coupling_gates)(nqubits - 1, 0))
+        gate_list.append(getattr(gates, coupling_gates)(n_qubits - 1, 0))
 
     return gate_list
