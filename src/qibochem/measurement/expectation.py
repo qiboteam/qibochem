d9989875de42d6e9710fb881dd4e1c92d832d4fd
Wong Zi Cheng <70616433+chmwzc@users.noreply.github.com>
2023-11-03 03:49:24 +0000

    Draft working expectation_from_samples code
    To test next

--- src/qibochem/measurement/expectation.py
+++ src/qibochem/measurement/expectation.py
@@ -1,8 +1,7 @@
-# from qibo import gates
-# from qibo.models import Circuit
-
 import qibo
+from qibo import gates
 from qibo.hamiltonians import SymbolicHamiltonian
+from qibo.symbols import Z
 
 from qibochem.measurement.basis_rotate import measure_rotate_basis
 
@@ -90,27 +89,34 @@ def expectation(
         Hamiltonian expectation value (float)
     """
     if from_samples:
-        raise NotImplementedError("expectation function only works with state vector")
-    # TODO: Rough code for expectation_from_samples if issue resolved
-    # Yet to test!!!!!
-    #
-    # from functools import reduce
-    # total = 0.0
-    # Iterate over each term in the Hamiltonian
-    # for term in hamiltonian.terms:
-    #     # Get the basis rotation gates and target qubits from the Hamiltonian term
-    #     qubits = [factor.target_qubit for factor in term.factors]
-    #     basis = [type(factor.gate) for factor in term.factors]
-    #     # Run a copy of the initial circuit to get the output frequencies
-    #     _circuit = circuit.copy()
-    #     _circuit.add(gates.M(*qubits, basis=basis))
-    #     result = _circuit(nshots=n_shots)
-    #     frequencies = result.frequencies(binary=True)
-    #     # Only works for Z terms, raises an error if ham_term has X/Y terms
-    #     total += SymbolicHamiltonian(
-    #                  reduce(lambda x, y: x*y, term.factors, 1)
-    #              ).expectation_from_samples(frequencies, qubit_map=qubits)
-    # return total
+        from functools import reduce
+
+        total = 0.0
+        # Iterate over each term in the Hamiltonian
+        for term in hamiltonian.terms:
+            # Get the target qubits and basis rotation gates from the Hamiltonian term
+            qubits = [int(factor.target_qubit) for factor in term.factors]
+            basis = [type(factor.gate) for factor in term.factors]
+            # Run a copy of the original circuit to get the output frequencies
+            _circuit = circuit.copy()
+            _circuit.add(gates.M(*qubits, basis=basis))
+            result = _circuit(nshots=n_shots)
+            frequencies = result.frequencies(binary=True)
+            # Only works for Z terms, raises an error if ham_term has X/Y terms
+            # total += SymbolicHamiltonian(
+            #              reduce(lambda x, y: x*y, term.factors, 1)
+            #          ).expectation_from_samples(frequencies, qubit_map=qubits)
+            # Workaround code to handle X/Y terms in the Hamiltonian:
+            # Get each Pauli string e.g. X0Y1
+            pauli = [factor.name for factor in term.factors]
+            # Replace each X and Y symbol with Z; then include the term coefficient
+            pauli_z = [Z(int(element[1:])) for element in pauli]
+            z_only_ham = SymbolicHamiltonian(term.coefficient * reduce(lambda x, y: x * y, pauli_z, 1.0))
+            # Can now apply expectation_from_samples directly
+            total += z_only_ham.expectation_from_samples(frequencies, qubit_map=qubits)
+        # Add the constant term if present. Note: Energies (in chemistry) are all real values
+        total += hamiltonian.constant.real
+        return total
 
     # Expectation value from state vector simulation
     result = circuit(nshots=1)
