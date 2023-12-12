c10e46a1c6d7b797a047910e957bb1b64d96b9d4
Wong Zi Cheng <70616433+chmwzc@users.noreply.github.com>
2023-12-12 08:41:05 +0000

    Re-factor expectation from samples code
    Eventually want to have some shot allocation for each term in the Hamiltonian
    Currently, the code is s.t. `n_shots` is allocated for every single term in the Hamiltonian

--- src/qibochem/measurement/expectation.py
+++ src/qibochem/measurement/expectation.py
@@ -1,11 +1,51 @@
+from functools import reduce
+
 import qibo
 from qibo import gates
 from qibo.hamiltonians import SymbolicHamiltonian
 from qibo.symbols import Z
 
 
+def pauli_term_sample_expectation(circuit, pauli_term, n_shots):
+    """
+    Calculate the expectation value of a general Pauli string for a given circuit ansatz and number of shots
+
+    Args:
+        circuit (qibo.models.Circuit): Quantum circuit ansatz
+        pauli_term (qibo.hamiltonians.terms.SymbolicTerm): General Pauli term
+        n_shots (int): Number of times the circuit is run
+
+    Returns:
+        Expectation value of the Pauli term (float)
+    """
+    # Get the target qubits and basis rotation gates from the Hamiltonian term
+    qubits = [int(factor.target_qubit) for factor in pauli_term.factors]
+    basis = [type(factor.gate) for factor in pauli_term.factors]
+    # Run a copy of the original circuit to get the output frequencies
+    _circuit = circuit.copy()
+    _circuit.add(gates.M(*qubits, basis=basis))
+    result = _circuit(nshots=n_shots)
+    frequencies = result.frequencies(binary=True)
+
+    # Only works for Z terms, raises an error if ham_term has X/Y terms
+    # single_term_ham = SymbolicHamiltonian(reduce(lambda x, y: x*y, pauli_term.factors, 1))
+    # return single_term_ham.expectation_from_samples(frequencies, qubit_map=qubits)
+
+    # Workaround code to handle X/Y terms in the Hamiltonian:
+    # Get each Pauli string e.g. X0Y1
+    pauli = [factor.name for factor in pauli_term.factors]
+    # Replace each X and Y symbol with Z; then include the term coefficient
+    pauli_z = [Z(int(element[1:])) for element in pauli]
+    z_only_ham = SymbolicHamiltonian(pauli_term.coefficient * reduce(lambda x, y: x * y, pauli_z, 1.0))
+    # Can now apply expectation_from_samples directly
+    return z_only_ham.expectation_from_samples(frequencies, qubit_map=qubits)
+
+
 def expectation(
-    circuit: qibo.models.Circuit, hamiltonian: SymbolicHamiltonian, from_samples=False, n_shots=1000
+    circuit: qibo.models.Circuit,
+    hamiltonian: SymbolicHamiltonian,
+    from_samples: bool = False,
+    n_shots: int = 1000,
 ) -> float:
     """
     Calculate expectation value of some Hamiltonian using either the state vector or sample measurements from running a
@@ -22,31 +62,7 @@ def expectation(
         Hamiltonian expectation value (float)
     """
     if from_samples:
-        from functools import reduce
-
-        total = 0.0
-        # Iterate over each term in the Hamiltonian
-        for term in hamiltonian.terms:
-            # Get the target qubits and basis rotation gates from the Hamiltonian term
-            qubits = [int(factor.target_qubit) for factor in term.factors]
-            basis = [type(factor.gate) for factor in term.factors]
-            # Run a copy of the original circuit to get the output frequencies
-            _circuit = circuit.copy()
-            _circuit.add(gates.M(*qubits, basis=basis))
-            result = _circuit(nshots=n_shots)
-            frequencies = result.frequencies(binary=True)
-            # Only works for Z terms, raises an error if ham_term has X/Y terms
-            # total += SymbolicHamiltonian(
-            #              reduce(lambda x, y: x*y, term.factors, 1)
-            #          ).expectation_from_samples(frequencies, qubit_map=qubits)
-            # Workaround code to handle X/Y terms in the Hamiltonian:
-            # Get each Pauli string e.g. X0Y1
-            pauli = [factor.name for factor in term.factors]
-            # Replace each X and Y symbol with Z; then include the term coefficient
-            pauli_z = [Z(int(element[1:])) for element in pauli]
-            z_only_ham = SymbolicHamiltonian(term.coefficient * reduce(lambda x, y: x * y, pauli_z, 1.0))
-            # Can now apply expectation_from_samples directly
-            total += z_only_ham.expectation_from_samples(frequencies, qubit_map=qubits)
+        total = sum(pauli_term_sample_expectation(circuit, term, n_shots) for term in hamiltonian.terms)
         # Add the constant term if present. Note: Energies (in chemistry) are all real values
         total += hamiltonian.constant.real
         return total
