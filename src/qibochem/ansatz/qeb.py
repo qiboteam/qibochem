e52f90a1ee47e710818a66dadb9935c9b9b88dfd
Adrian Mak <damarkian@gmail.com>
2024-09-03 11:53:08 +0800

    added docstring for qeb and updated doc/api-reference

--- src/qibochem/ansatz/qeb.py
+++ src/qibochem/ansatz/qeb.py
@@ -3,6 +3,23 @@ import qibo
 from qibo import Circuit, models, gates
 
 def qeb_circuit(n_qubits, excitation, theta=0.0, trotter_steps=1) -> Circuit:
+    r"""
+    Qubit-excitation-based (QEB) circuit corresponding to the unitary coupled-cluster ansatz for a single excitation
+
+    Supports only Jordan-Wigner encoded circuits
+
+    Ref: arXiv:2210.05771
+
+    Args:
+        n_qubits: Number of qubits in the quantum circuit
+        excitation: Iterable of orbitals involved in the excitation; must have an even number of elements
+            E.g. ``[0, 1, 2, 3]`` represents the excitation of electrons in orbitals ``(0, 1)`` to ``(2, 3)``
+        theta: UCC parameter. Defaults to 0.0
+        trotter_steps: Number of Trotter steps; i.e. number of times this ansatz is applied with ``theta`` = ``theta / trotter_steps``. Default: 1
+
+    Returns:
+        Qibo ``Circuit``: Circuit corresponding to a single UCC excitation
+    """
     
     n_orbitals = len(excitation)
     assert n_orbitals % 2 == 0, f"{excitation} must have an even number of items"
