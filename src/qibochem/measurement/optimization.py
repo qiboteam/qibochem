a57aaf7e6cabec77af074c5e97f0637fc1699675
Ah Wong <70616433+chmwzc@users.noreply.github.com>
2026-03-19 15:08:04 +0800

    Sketch out GC measurement structure

--- src/qibochem/measurement/optimization.py
+++ src/qibochem/measurement/optimization.py
@@ -49,13 +49,45 @@ def qwc_measurement_gates(expression):
     return list(m_gates.values())
 
 
-def qwc_measurements(hamiltonian):
+def gc_measurement_gates(expression):
+    """
+    Get the list of (basis rotation) measurement gates to be added to the circuit. The measurements from the resultant
+    circuit can then be used to obtain the expectation values of ALL the terms in expression directly.
+
+    Args:
+        expression (sympy.Expr): Group of Pauli terms that all mutually commute with each other qubitwise
+
+    Returns:
+        list: Measurement gates to be appended to the Qibo circuit
+    """
+
+    # m_gates, _m_gates = {}, {}
+    # # Single Pauli operator
+    # if not expression.args:
+    #     return [gates.M(expression.target_qubit, basis=type(expression.gate))]
+    # # Either a single Pauli term or a sum of Pauli terms
+    # for term in expression.args:
+    #     # Term should either be a single Pauli operator or a Pauli string
+    #     if isinstance(term, (X, Y, Z)):
+    #         _m_gates = {term.target_qubit: gates.M(term.target_qubit, basis=type(term.gate))}
+    #     else:
+    #         _m_gates = {
+    #             pauli_op.target_qubit: gates.M(pauli_op.target_qubit, basis=type(pauli_op.gate))
+    #             for pauli_op in term.args
+    #             if m_gates.get(pauli_op.target_qubit) is None
+    #         }
+    #     m_gates = {**m_gates, **_m_gates}
+    return  # list(m_gates.values())
+
+
+def grouped_measurements(hamiltonian, generally_commuting=False):
     """
     Sort out a list of Hamiltonian terms into separate groups of mutually qubitwise commuting terms, and returns the
     grouped terms along with their associated measurement gates
 
     Args:
         hamiltonian (:class:`qibo.hamiltonians.SymbolicHamiltonian`): Hamiltonian of interest
+        generally_commuting (bool): "GC" or "QWC" measurements. Default (False) is "QWC"
 
     Returns:
         list: List of two-tuples, with each tuple given as (sorted_ham, [`list of measurement gates`]), where
@@ -70,11 +102,12 @@ def qwc_measurements(hamiltonian):
         }
     else:
         ham_terms = {term_to_string(hamiltonian.form): (hamiltonian.form, 1.0)}  # Single Pauli operator
-    term_groups = group_commuting_terms(ham_terms.keys(), qubitwise=True)
+    term_groups = group_commuting_terms(ham_terms.keys(), qubitwise=(not generally_commuting))
+    measurement_gates_fn = qwc_measurement_gates if not generally_commuting else gc_measurement_gates
     return [
         (
             sum(ham_terms[term][1] * ham_terms[term][0] for term in term_group),  # Original expression: coeff*term
-            qwc_measurement_gates(sum(ham_terms[term][0] for term in term_group)),  # No coeff for qwc_measurement_gates
+            measurement_gates_fn(sum(ham_terms[term][0] for term in term_group)),  # No coeff for qwc_measurement_gates
         )
         for term_group in term_groups
     ]
@@ -105,7 +138,9 @@ def measurement_basis_rotations(hamiltonian, grouping=None):
             if not isinstance(term, One)  # Ignore any constant term
         ]
     elif grouping == "qwc":
-        result += qwc_measurements(hamiltonian)
+        result += grouped_measurements(hamiltonian, False)
+    elif grouping == "gc":
+        result += grouped_measurements(hamiltonian, True)
     else:
         raise NotImplementedError("Not ready yet!")
     return result
