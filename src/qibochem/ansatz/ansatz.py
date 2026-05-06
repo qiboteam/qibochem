7cd23e3297b06a6e99e1e6414d26d29ec9c6a387
Ah Wong <70616433+chmwzc@users.noreply.github.com>
2026-05-06 17:00:21 +0800

    Add hardware efficient ansatz

--- src/qibochem/ansatz/ansatz.py
+++ src/qibochem/ansatz/ansatz.py
@@ -2,12 +2,65 @@
 Circuit ansatzes for chemistry
 """
 
-import numpy as np
-from qibo import Circuit
+from collections.abc import Sequence
+
+from qibo import Circuit, gates
 from qibo.config import raise_error
-from qibo.models.encodings import comp_basis_encoder
+from qibo.gates import Gate
+from qibo.models.encodings import comp_basis_encoder, entangling_layer
 
 from qibochem.ansatz._ansatz import _bk_matrix
+
+
+def he_circuit(
+    nqubits: int,
+    nlayers: int,
+    parameter_gates: Iterable[str | Gate] | None = None,
+    entangling_gate: str | Gate = "CNOT",
+    architecture: str = "diagonal",
+    closed_boundary: bool = True,
+    **kwargs,
+) -> Circuit:
+    """
+    Builds a general hardware-efficient ansatz, in which the rotation and entangling gates used can be chosen by the
+    user. For more details on the arguments related to the entangling layer, see the documentation for
+    :class:`qibo.models.encodings.entangling_layer`.
+
+    Args:
+        nqubits (int): Number of qubits in the quantum circuit.
+        nlayers (int): Number of layers of rotation and entangling gates.
+        parameter_gates (Iterable[str | Gate] | None, optional): Single-qubit rotation gates used in the ansatz. These
+            can be given as strings representing valid one-qubit gates, or as :class:`qibo.gates.Gate` directly.
+            Default: ``["RY", "RZ"]``
+        entangling_gate (str | Gate, optional): Two-qubit entangling gate used in the ansatz. This can be given as
+            strings representing valid two-qubit gates, or as a :class:`qibo.gates.Gate` directly. Default: ``"CNOT"``
+        architecture (str, optional): Architecture of the entangling layer, with the possible options: ``"diagonal"``,
+            ``"even_layer"``, ``"next_nearest"``, ``"odd_layer"``, ``"pyramid"``, ``"shifted"``, ``"v"``, and ``"x"``
+            (defined only for an even number of qubits. Default: ``"diagonal"``
+        closed_boundary (bool, optional): If ``True`` (default) and ``architecture not in ["pyramid", "v", "x"]``, adds
+            a closed-boundary condition to the entangling layer
+        kwargs (dict, optional): Additional arguments used to initialize a Circuit object. Details are given in the
+            documentation of :class:`qibo.models.circuit.Circuit`.
+
+    Returns:
+        :class:`qibo.models.circuit.Circuit`: Circuit corresponding to the hardware-efficient ansatz
+    """
+    # Default variables
+    if parameter_gates is None:
+        parameter_gates = ["RY", "RZ"]
+    parameter_gates = [getattr(gates, _gate) if isinstance(_gate, str) else _gate for _gate in parameter_gates]
+
+    circuit = Circuit(nqubits, **kwargs)
+    for _ in range(nlayers):
+        # Rotation gates
+        circuit.add(
+            rgate(qubit, theta=0.0)  # pylint: disable=not-callable
+            for qubit in range(nqubits)
+            for rgate in parameter_gates
+        )
+        # Entangling gates
+        circuit += entangling_layer(nqubits, architecture, entangling_gate, closed_boundary, **kwargs)
+    return circuit
 
 
 def hf_circuit(nqubits: int, nelectrons: int, ferm_qubit_map: str | None = None, **kwargs) -> Circuit:
@@ -16,8 +69,8 @@ def hf_circuit(nqubits: int, nelectrons: int, ferm_qubit_map: str | None = None,
     Args:
         nqubits (int): Number of qubits in the quantum circuit
         nelectrons (int): Number of electrons in the molecular system
-        ferm_qubit_map (str | None, optional): Fermion to qubit map. Must be either Jordan-Wigner (``"jw"``) or Brayvi-Kitaev
-            (``"bk"``). Default value is ``"jw"``.
+        ferm_qubit_map (str | None, optional): Fermion to qubit map. Must be either Jordan-Wigner (``"jw"``) or
+            Brayvi-Kitaev (``"bk"``). Default value is ``"jw"``.
         kwargs (dict, optional): Additional arguments used to initialize a Circuit object. Details are given in the
             documentation of :class:`qibo.models.circuit.Circuit`.
 
